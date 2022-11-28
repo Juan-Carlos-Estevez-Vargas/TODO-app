2c779e0856dafa5432405b0d83ef65e213acade1
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-11-28 10:27:00 -0500

    Adicionando las tareas del LocalStorage a la pantalla.

--- components/addTask.js
+++ components/addTask.js
@@ -2,41 +2,45 @@ import checkComplete from "./checkComplete.js";
 import deleteIcon from "./deleteIcon.js";
 
 export const addTask = (evento) => {
-  const list = document.querySelector("[data-list]");
-  const task = createTask(evento);
-  list.appendChild(task);
-};
-
-const createTask = (evento) => {
   evento.preventDefault();
 
-  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-
+  const list = document.querySelector("[data-list]");
   const input = document.querySelector("[data-form-input]");
-  const value = input.value;
-
   const calendar = document.querySelector("[data-form-date]");
+
+  const value = input.value;
   const date = calendar.value;
   const dateFormat = moment(date).format("DD/MM/YYYY");
 
-  const task = document.createElement("li");
-  task.classList.add("card");
-  input.value = "";
+  if (value === "" || date === "") {
+    return;
+  }
 
-  const taskContent = document.createElement("div");
+  input.value = "";
+  calendar.value = "";
 
   const taskObject = {
     value,
     dateFormat,
   };
 
+  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
   taskList.push(taskObject);
 
   localStorage.setItem("tasks", JSON.stringify(taskList));
 
-  const titleTask = document.createElement("span");
+  const task = createTask(taskObject);
+  list.appendChild(task);
+};
 
+export const createTask = ({ value, dateFormat }) => {
+  const task = document.createElement("li");
+  task.classList.add("card");
+
+  const taskContent = document.createElement("div");
+  const titleTask = document.createElement("span");
   const dateElement = document.createElement("span");
+
   dateElement.innerHTML = dateFormat;
 
   titleTask.classList.add("task");
